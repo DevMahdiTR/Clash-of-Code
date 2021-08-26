960bcdcc4954140b21243903bb99cb336c8dd35e
DevMahdiTR <62462645+DevMahdiTR@users.noreply.github.com>
2021-08-26 15:44:17 -0700

    Create Teacher's Reward.py

---
+++ Shortes/Teacher's Reward.py
@@ -0,0 +1,7 @@
+# The teacher John decided to give sweets to students .
+# He will distribute the sweets to students who passed 
+# the exam in the order of 1 sweet for first student 3 
+# sweets for second student 5 sweets for third student and so on ...
+# HOW MANY BAG DOES THE TEACHER NEEDS knowing that the bag has 10 pieces.
+
+print(-(-int(input())**2//10)) # sum of odd number n²
